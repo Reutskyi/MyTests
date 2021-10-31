cb83105a81b0ea573464d54c7c6de0dd5f4c1656
Igor Reutskyi <Igor.Reutskyi@redsailtechnologies.com>
2021-10-31 17:08:15 +0200

    Updated test

--- AmazonTest/Amazon.cs
+++ AmazonTest/Amazon.cs
@@ -31,6 +31,19 @@ namespace AmazonTest
             var Search = driver.FindElement(By.Id("nav-search-submit-button"));
             Search.Click();
 
+            bool resultSearch;
+
+            try
+            {
+                resultSearch = driver.FindElement(By.XPath("//span[@class = 'rush-component']//span[contains(text(), 'results for')]")).Displayed;
+            }
+            catch
+            {
+                resultSearch = false;
+            }
+
+            Assert.IsTrue(resultSearch, "Page isn't displayed");
+
             wait.Until(condition =>
             {
                 try
@@ -43,13 +56,38 @@ namespace AmazonTest
                     return false;
                 }
             });
-
+        
             driver.FindElement(By.XPath("//div[@data-cel-widget='search_result_0']//a[text()='Paperback'][1]")).Click();
 
             driver.FindElement(By.Id("add-to-cart-button")).Click();
 
             driver.FindElement(By.Id("nav-cart")).Click();
 
+            bool shoppingCart;
+
+            try
+            {
+                shoppingCart = driver.FindElement(By.XPath("//div[@id = 'sc-active-cart']//h1[contains(text(), 'Shopping Cart')]")).Displayed;
+            }
+            catch
+            {
+                shoppingCart = false;
+            }
+
+            Assert.IsTrue(shoppingCart, "Page isn't displayed");
+
+            bool emptyCart;
+
+            try
+            {
+                emptyCart = driver.FindElement(By.XPath("//div[@id = 'sc-active-cart']//h2[contains(text(), 'Cart is empty')]")).Displayed;
+            }
+            catch
+            {
+                emptyCart = false;
+            }
+
+            Assert.IsFalse(emptyCart, "Cart isn't empty");
         }
 
         [TearDown]
